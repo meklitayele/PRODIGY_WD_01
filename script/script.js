4947b1ed83f704bba3d5cfdf6b30ca9a882b5a97
Meklit <meklitmelkamu34@yahoo.com>
2024-04-26 19:40:55 +0300

    update

---
+++ script/script.js
@@ -0,0 +1,11 @@
+
+let arrowRight = document.querySelectorAll('.material-symbols-outlined')
+
+arrowRight.forEach((e) => {
+    e.addEventListener('click', createAnimation)
+})
+
+function createAnimation() {
+
+
+}
