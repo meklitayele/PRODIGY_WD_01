8656eb0dd3c2f674f567f127557a8573974a327a
Meklit Habtamu <124443510+meklithab@users.noreply.github.com>
2024-06-12 16:52:39 +0300

    Update hotels2.php

--- main/ADDIS/hotels2.php
+++ main/ADDIS/hotels2.php
@@ -2,7 +2,7 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "travel";
+$dbname = "travel2";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 ?>
@@ -72,7 +72,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
         </div>
       </a>
       <div class="rating">
-        <span class="rating-label">Rating:</span>
+        <span class="rating-label">Rating</span>
         <span class="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
       </div>
     </div>
@@ -101,146 +101,27 @@ $conn = new mysqli($servername, $username, $password, $dbname);
       <div class="visit-hotel">
         <a href="https://www.hilton.com/en/hotels/addhitw-hilton-addis-ababa/?WT.mc_id=zINDA0EMEA1MB2PSH3GGL4INTBPP5dkt6MULTIBR7_121127567_1003528_p7949166987&gclsrc=3p.ds&&&&&&gclid=09383deac1d6138299d5584916ebb24c&gclsrc=3p.ds&msclkid=09383deac1d6138299d5584916ebb24c" target="_blank">Visit Hotel Website</a>
         <a href="../Hotels/reservation.php">Make a reservation</a>
-        <a href="view_review.php">View Reviews</a>
+        <a href="view_review.php?hotel_name=Hilton Hotel" target="_blank">View Reviews</a>
       </div>
 
-      <?php
-      $sql = "
-    SELECT
-        h.hotel_name,
-        r.review_text,
-        r.review_rating,
-        r.review_date
-    FROM hotels h
-    JOIN hotel_reviews r ON h.hotel_id = r.hotel_id
-    JOIN cities c ON h.city_id = c.city_id
-    WHERE c.city_name = 'Addis Ababa' AND h.hotel_name = 'Hilton Hotel'
-    ORDER BY r.review_date DESC;
-    ";
 
-      $result = $conn->query($sql);
-
-      if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-          echo "<p> Review: " . $row["review_text"] . "</p>";
-          echo "<p>Rating: " . $row["review_rating"] . " / 5.0</p>";
-          echo "<p>Review Date: " . $row["review_date"] . "</p>";
-          echo "<hr>";
-          echo "<br/>";
-        }
-      } else {
-        echo "No reviews found.";
-      }
-      ?>
-
-
-
-      <div class="hotel-display">
-        <a href="https://www.bing.com/aclick?ld=e8y0JlRZiBabW63uHUl_j68DVUCUwnwgoFsurQTIBHxQgx2W2w2Bwgkaz5opKaLyq0xS0ZB48TDYa7C7H-czxBp3ck2MlSVEfX6muCy1HgCNfqnIhP9VYQNGKHoetj7DcOYu6FTJ_t4kQL_qOktKSLftauiqz8N-ggxeoDTZbY4Puzh2YNHSiBronboGd_zXwTHmYABg&u=aHR0cHMlM2ElMmYlMmZ3d3cubWFycmlvdHQuY29tJTJmZGVmYXVsdC5taSUzZm5zdCUzZHBhaWQlMjZjaWQlM2RQQUlfR0xCMDAwNEZQOF9HTEUwMDBCTTZZX0dMRjAwME9QRFUlMjZwcGMlM2RwcGMlMjZwSWQlM2Rjb3JwaW1lZGlhJTI2Z2NsaWQlM2RlN2MzYzFjZjg4NTYxN2U5N2M5YWE0NmM1ZGZhZjA5NSUyNmdjbHNyYyUzZDNwLmRzJTI2&rlid=e7c3c1cf885617e97c9aa46c5dfaf095&ntb=1&ntb=1" target="_blank">
-          <div class="hotel-image">
-            <img src="images/sheraton.jpg" alt="Hotel Image" />
-          </div>
-          <div class="hotel-details">
-            <h2 class="hotel-name">Sheraton Hotel</h2>
-            <p class="hotel-description">
-              Sheraaton is a five star hotel, located close to the commercial
-              center of the city. The hotel facility consists of bar, restaurant,
-              swimming pool, and beautiful garden to sit and relax. It is one of
-              the best recommended hotel by most visitors. Our service is personal
-              and sincere, offering a luxury experience from the moment you
-              touchdown in Jimma, Ethiopia.
-            </p>
-            <div class="services">
-              <h3>Services</h3>
-              <ul>
-                <li>Free Wi-Fi</li>
-                <li>Swimming Pool</li>
-                <li>Spa and Wellness Center</li>
-                <li>Restaurant and Bar</li>
-                <li>24-hour Room Service</li>
-              </ul>
-            </div>
-          </div>
-        </a>
-        <div class="rating">
-          <span class="rating-label">Rating:</span>
-          <span class="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
-        </div>
-      </div>
-      <div class="under">
-
-        <h3>Leave a Review</h3>
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-          <input class="hidden" name="hotel_id" value="2">
-          <div>
-            <label for="review_text">Review:</label>
-            <textarea name="review_text" id="review_text" rows="5"></textarea>
-
-          </div>
-          <div class="rating">
-            <input type="radio" id="star5" name="review_rating" value="5" />
-            <label for="star5">5 stars</label>
-            <input type="radio" id="star4" name="review_rating" value="4" />
-            <label for="star4">4 stars</label>
-            <input type="radio" id="star3" name="review_rating" value="3" />
-            <label for="star3">3 stars</label>
-            <input type="radio" id="star2" name="review_rating" value="2" />
-            <label for="star2">2 stars</label>
-            <input type="radio" id="star1" name="review_rating" value="1" />
-            <label for="star1">1 star</label>
-          </div>
-          <button type="submit" style="background:#2d1e18; color:white;  padding: 8px 15px; border-radius:6px;">Submit Review</button>
-        </form>
-        <div class="visit-hotel">
-          <a href="https://www.bing.com/aclick?ld=e8y0JlRZiBabW63uHUl_j68DVUCUwnwgoFsurQTIBHxQgx2W2w2Bwgkaz5opKaLyq0xS0ZB48TDYa7C7H-czxBp3ck2MlSVEfX6muCy1HgCNfqnIhP9VYQNGKHoetj7DcOYu6FTJ_t4kQL_qOktKSLftauiqz8N-ggxeoDTZbY4Puzh2YNHSiBronboGd_zXwTHmYABg&u=aHR0cHMlM2ElMmYlMmZ3d3cubWFycmlvdHQuY29tJTJmZGVmYXVsdC5taSUzZm5zdCUzZHBhaWQlMjZjaWQlM2RQQUlfR0xCMDAwNEZQOF9HTEUwMDBCTTZZX0dMRjAwME9QRFUlMjZwcGMlM2RwcGMlMjZwSWQlM2Rjb3JwaW1lZGlhJTI2Z2NsaWQlM2RlN2MzYzFjZjg4NTYxN2U5N2M5YWE0NmM1ZGZhZjA5NSUyNmdjbHNyYyUzZDNwLmRzJTI2&rlid=e7c3c1cf885617e97c9aa46c5dfaf095&ntb=1&ntb=1" target="_blank">Visit Hotel Website</a> <a href="../Hotels/reservation.php">Make a reservation</a>
-          <a href="view_review.php">View Reviews</a>
-        </div>
-      </div>
     </div>
-    <?php
-    $sql = "
-    SELECT
-        h.hotel_name,
-        r.review_text,
-        r.review_rating,
-        r.review_date
-    FROM hotels h
-    JOIN hotel_reviews r ON h.hotel_id = r.hotel_id
-    JOIN cities c ON h.city_id = c.city_id
-    WHERE c.city_name = 'Addis Ababa' AND h.hotel_name = 'Sherton Hotel'
-    ORDER BY r.review_date DESC;
-    ";
-
-    $result = $conn->query($sql);
-
-    if ($result->num_rows > 0) {
-      while ($row = $result->fetch_assoc()) {
-        echo "<p> Review: " . $row["review_text"] . "</p>";
-        echo "<p>Rating: " . $row["review_rating"] . " / 5.0</p>";
-        echo "<p>Review Date: " . $row["review_date"] . "</p>";
-        echo "<hr>";
-        echo "<br/>";
-      }
-    } else {
-      echo "No reviews found.";
-    }
-    ?>
 
 
     <div class="hotel-display">
-      <a href="https://www.hyatt.com/en-US/hotel/ethiopia/hyatt-regency-addis-ababa/addra" target="_blank">
+      <a href="https://www.bing.com/aclick?ld=e8y0JlRZiBabW63uHUl_j68DVUCUwnwgoFsurQTIBHxQgx2W2w2Bwgkaz5opKaLyq0xS0ZB48TDYa7C7H-czxBp3ck2MlSVEfX6muCy1HgCNfqnIhP9VYQNGKHoetj7DcOYu6FTJ_t4kQL_qOktKSLftauiqz8N-ggxeoDTZbY4Puzh2YNHSiBronboGd_zXwTHmYABg&u=aHR0cHMlM2ElMmYlMmZ3d3cubWFycmlvdHQuY29tJTJmZGVmYXVsdC5taSUzZm5zdCUzZHBhaWQlMjZjaWQlM2RQQUlfR0xCMDAwNEZQOF9HTEUwMDBCTTZZX0dMRjAwME9QRFUlMjZwcGMlM2RwcGMlMjZwSWQlM2Rjb3JwaW1lZGlhJTI2Z2NsaWQlM2RlN2MzYzFjZjg4NTYxN2U5N2M5YWE0NmM1ZGZhZjA5NSUyNmdjbHNyYyUzZDNwLmRzJTI2&rlid=e7c3c1cf885617e97c9aa46c5dfaf095&ntb=1&ntb=1" target="_blank">
         <div class="hotel-image">
-          <img src=".\images/hayat.jpg " alt="Hotel Image" />
+          <img src="images/sheraton.jpg" alt="Hotel Image" />
         </div>
         <div class="hotel-details">
-          <h2 class="hotel-name">Hyat Regency</h2>
+          <h2 class="hotel-name">Sheraton Hotel</h2>
           <p class="hotel-description">
-            Hyatt Regency is located just 15 minutes from Bole international
-            airport, and most centrally located on Meskel Square, connecting the
-            large artery roads of Addis Ababa. The hotel stands within a short
-            walking distance to the United Nations Conference center and within
-            a 15 min. drive to the African Union Headquarters with the largest
-            conference facilities in the country.
+            Sheraton is a five star hotel, located close to the commercial
+            center of the city. The hotel facility consists of bar, restaurant,
+            swimming pool, and beautiful garden to sit and relax. It is one of
+            the best recommended hotel by most visitors. Our service is personal
+            and sincere, offering a luxury experience from the moment you
+            touchdown in Jimma, Ethiopia.
           </p>
           <div class="services">
             <h3>Services</h3>
@@ -255,26 +136,19 @@ $conn = new mysqli($servername, $username, $password, $dbname);
         </div>
       </a>
       <div class="rating">
-        <span class="rating-label">Rating:</span>
+        <span class="rating-label">Rating</span>
         <span class="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
       </div>
     </div>
     <div class="under">
 
-      <!-- <div class="review">
-          <label for="review-text">Write a review:</label>
-          <textarea
-            id="review-text"
-            class="review-textarea"
-            rows="4"
-          ></textarea>
-        </div> -->
       <h3>Leave a Review</h3>
       <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-        <input class="hidden" name="hotel_id" value="3">
+        <input class="hidden" name="hotel_id" value="2">
         <div>
           <label for="review_text">Review:</label>
-          <textarea name="review_text" id="review_text" rows="5" required></textarea>
+          <textarea name="review_text" id="review_text" rows="5"></textarea>
+
         </div>
         <div class="rating">
           <input type="radio" id="star5" name="review_rating" value="5" />
@@ -291,40 +165,84 @@ $conn = new mysqli($servername, $username, $password, $dbname);
         <button type="submit" style="background:#2d1e18; color:white;  padding: 8px 15px; border-radius:6px;">Submit Review</button>
       </form>
       <div class="visit-hotel">
-        <a href="https://www.hyatt.com/en-US/hotel/ethiopia/hyatt-regency-addis-ababa/addra" target="_blank">Visit Hotel Website</a>
-        <a href="../Hotels/reservation.php">Make a reservation</a>
-        <a href="view_review.php">View Reviews</a>
+        <a href="https://www.bing.com/aclick?ld=e8y0JlRZiBabW63uHUl_j68DVUCUwnwgoFsurQTIBHxQgx2W2w2Bwgkaz5opKaLyq0xS0ZB48TDYa7C7H-czxBp3ck2MlSVEfX6muCy1HgCNfqnIhP9VYQNGKHoetj7DcOYu6FTJ_t4kQL_qOktKSLftauiqz8N-ggxeoDTZbY4Puzh2YNHSiBronboGd_zXwTHmYABg&u=aHR0cHMlM2ElMmYlMmZ3d3cubWFycmlvdHQuY29tJTJmZGVmYXVsdC5taSUzZm5zdCUzZHBhaWQlMjZjaWQlM2RQQUlfR0xCMDAwNEZQOF9HTEUwMDBCTTZZX0dMRjAwME9QRFUlMjZwcGMlM2RwcGMlMjZwSWQlM2Rjb3JwaW1lZGlhJTI2Z2NsaWQlM2RlN2MzYzFjZjg4NTYxN2U5N2M5YWE0NmM1ZGZhZjA5NSUyNmdjbHNyYyUzZDNwLmRzJTI2&rlid=e7c3c1cf885617e97c9aa46c5dfaf095&ntb=1&ntb=1" target="_blank">Visit Hotel Website</a> <a href="../Hotels/reservation.php">Make a reservation</a>
+        <a href="view_review.php?hotel_name=Sheraton Hotel" target="_blank">View Reviews</a>
       </div>
-      <?php
-      $sql = "
-    SELECT
-        h.hotel_name,
-        r.review_text,
-        r.review_rating,
-        r.review_date
-    FROM hotels h
-    JOIN hotel_reviews r ON h.hotel_id = r.hotel_id
-    JOIN cities c ON h.city_id = c.city_id
-    WHERE c.city_name = 'Addis Ababa' AND h.hotel_name = 'Hyatt Regency'
-    ORDER BY r.review_date DESC;
-    ";
-
-      $result = $conn->query($sql);
-
-      if ($result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-          echo "<p> Review: " . $row["review_text"] . "</p>";
-          echo "<p>Rating: " . $row["review_rating"] . " / 5.0</p>";
-          echo "<p>Review Date: " . $row["review_date"] . "</p>";
-          echo "<hr>";
-          echo "<br/>";
-        }
-      } else {
-        echo "No reviews found.";
-      }
-      ?>
-
     </div>
+  </div>
+
+
+  <div class="hotel-display">
+    <a href="https://www.hyatt.com/en-US/hotel/ethiopia/hyatt-regency-addis-ababa/addra" target="_blank">
+      <div class="hotel-image">
+        <img src=".\images/hayat.jpg " alt="Hotel Image" />
+      </div>
+      <div class="hotel-details">
+        <h2 class="hotel-name">Hyat Regency</h2>
+        <p class="hotel-description">
+          Hyatt Regency is located just 15 minutes from Bole international
+          airport, and most centrally located on Meskel Square, connecting the
+          large artery roads of Addis Ababa. The hotel stands within a short
+          walking distance to the United Nations Conference center and within
+          a 15 min. drive to the African Union Headquarters with the largest
+          conference facilities in the country.
+        </p>
+        <div class="services">
+          <h3>Services</h3>
+          <ul>
+            <li>Free Wi-Fi</li>
+            <li>Swimming Pool</li>
+            <li>Spa and Wellness Center</li>
+            <li>Restaurant and Bar</li>
+            <li>24-hour Room Service</li>
+          </ul>
+        </div>
+      </div>
+    </a>
+    <div class="rating">
+      <span class="rating-label">Rating</span>
+      <span class="stars">&#9733;&#9733;&#9733;&#9733;&#9733;</span>
+    </div>
+  </div>
+  <div class="under">
+
+    <!-- <div class="review">
+          <label for="review-text">Write a review:</label>
+          <textarea
+            id="review-text"
+            class="review-textarea"
+            rows="4"
+          ></textarea>
+        </div> -->
+    <h3>Leave a Review</h3>
+    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+      <input class="hidden" name="hotel_id" value="3">
+      <div>
+        <label for="review_text">Review:</label>
+        <textarea name="review_text" id="review_text" rows="5" required></textarea>
+      </div>
+      <div class="rating">
+        <input type="radio" id="star5" name="review_rating" value="5" />
+        <label for="star5">5 stars</label>
+        <input type="radio" id="star4" name="review_rating" value="4" />
+        <label for="star4">4 stars</label>
+        <input type="radio" id="star3" name="review_rating" value="3" />
+        <label for="star3">3 stars</label>
+        <input type="radio" id="star2" name="review_rating" value="2" />
+        <label for="star2">2 stars</label>
+        <input type="radio" id="star1" name="review_rating" value="1" />
+        <label for="star1">1 star</label>
+      </div>
+      <button type="submit" style="background:#2d1e18; color:white;  padding: 8px 15px; border-radius:6px;">Submit Review</button>
+    </form>
+    <div class="visit-hotel">
+      <a href="https://www.hyatt.com/en-US/hotel/ethiopia/hyatt-regency-addis-ababa/addra" target="_blank">Visit Hotel Website</a>
+      <a href="../Hotels/reservation.php">Make a reservation</a>
+      <a href="view_review.php?hotel_name=Hyatt Regency" target="_blank">View Reviews</a>
+    </div>
+
+
+  </div>
   </div>
 </body>
 
